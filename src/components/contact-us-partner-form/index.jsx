880d584bdf0112cce859dfd677052b0aceb28e3a
Anton Titov <anton.titov@ukrainian-it.family>
2022-10-28 14:06:40 +0300

    feat: contacts page

---
+++ src/components/contact-us-partner-form/index.jsx
@@ -0,0 +1,100 @@
+import MainButton from '@components/main-button';
+import { yupResolver } from '@hookform/resolvers/yup';
+import styles from '@styles/components/contact-us-partner-form.module.scss';
+import { useCallback } from 'react';
+import { useForm } from 'react-hook-form';
+import * as yup from 'yup';
+
+const schema = yup.object({
+  name: yup.string().required('Required field'),
+  email: yup.string().email('Email must be valid').required('Required field'),
+  description: yup.string(),
+});
+
+const ContactUsPartnerForm = () => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const onSubmit = useCallback((formData) => {
+    const data = new FormData();
+
+    Object.keys(formData).forEach((key) => {
+      data.append(key, formData[key]);
+    });
+
+    console.log(formData);
+  }, []);
+
+  return (
+    <form className={styles.form} onSubmit={handleSubmit(onSubmit)}>
+      <div className={styles.form__content}>
+        <div className={styles['form__content-title-container']}>
+          <div className={styles['form__content-title']}>
+            Or fill the form<span className={styles['form__content-title-dot']}>.</span>
+          </div>
+          <div className={styles['form__content-subtitle']}>
+            Drop us a line and we’ll contact you as soon as possible
+          </div>
+        </div>
+
+        <div className={styles['form__content-field']}>
+          <label className={styles['form__content-field-label']} htmlFor="name">
+            Your name<span className={styles['form__content-field-required']}>*</span>
+          </label>
+          <input
+            className={`${styles['form__content-field-input']} ${errors?.name ? styles.error : ''}`}
+            id="name"
+            placeholder="Enter your name"
+            {...register('name')}
+          />
+
+          {errors?.name && <div className={styles['form__field-error']}>{errors.name.message}</div>}
+        </div>
+
+        <div className={styles['form__content-field']}>
+          <label className={styles['form__content-field-label']} htmlFor="email">
+            Your email<span className={styles['form__content-field-required']}>*</span>
+          </label>
+          <input
+            className={`${styles['form__content-field-input']} ${
+              errors?.email ? styles.error : ''
+            }`}
+            id="email"
+            placeholder="example@email.com"
+            {...register('email')}
+          />
+
+          {errors?.email && (
+            <div className={styles['form__field-error']}>{errors.email.message}</div>
+          )}
+        </div>
+
+        <div className={styles['form__content-field']}>
+          <label className={styles['form__content-field-label']} htmlFor="description">
+            Onboard us into your project
+          </label>
+          <textarea
+            className={`${styles['form__content-field-input']} ${
+              errors?.description ? styles.error : ''
+            }`}
+            id="description"
+            {...register('description')}
+          />
+
+          {errors?.description && (
+            <div className={styles['form__field-error']}>{errors.email.message}</div>
+          )}
+        </div>
+      </div>
+
+      <MainButton type="submit" width="100%" text="send" size="big" />
+    </form>
+  );
+};
+
+export default ContactUsPartnerForm;
--- src/pages/contacts.jsx
+++ src/pages/contacts.jsx
@@ -1,6 +1,48 @@
+import Breadcrumbs from '@components/breadcrumbs';
+import ContactUsPartnerForm from '@components/contact-us-partner-form';
+import ContactsList from '@components/contacts-list';
+import Footer from '@components/footer';
+import Header from '@components/header';
+import Layout from '@components/layout';
+import TitleSectionWrapper from '@components/title-section-wrapper';
+import styles from '@styles/pages/contacts.module.scss';
 import Head from 'next/head';
-import Image from 'next/image';
+import { useMemo } from 'react';
 
-const Contacts = () => <div>Contacts</div>;
+const Contacts = () => {
+  const crumbs = useMemo(() => [{ link: '/', text: 'Homepage' }, 'Contacts'], []);
+
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>Contacts | Ukrainian-IT.Family</title>
+        <meta name="description" content="" />
+      </Head>
+
+      <Header />
+
+      <Layout>
+        <Breadcrumbs crumbs={crumbs} centered />
+      </Layout>
+
+      <Layout>
+        <TitleSectionWrapper
+          title="Get in touch with UITF"
+          description="Drop us a line and we’ll contact you as soon as possible"
+        >
+          <ContactsList />
+        </TitleSectionWrapper>
+      </Layout>
+
+      <Layout filled>
+        <div className={styles['form-wrapper']}>
+          <ContactUsPartnerForm />
+        </div>
+      </Layout>
+
+      <Footer />
+    </div>
+  );
+};
 
 export default Contacts;
